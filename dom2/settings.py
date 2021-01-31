b123520548d144d3047b038ab4fba53f24c97573
Bogdan <soaqa57@gmail.com>
2021-01-31 17:18:12 +0300

    media

--- dom2/settings.py
+++ dom2/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'news',
-    'media'
+    'media',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
@@ -70,6 +71,14 @@ TEMPLATES = [
         },
     },
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 WSGI_APPLICATION = 'dom2.wsgi.application'
 
---
+++ news/models.py
@@ -0,0 +1,14 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+# Create your models here.
+class Novelty(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Заголовок", null=False, blank=True)
+    image = models.FileField(upload_to='images/', unique=True, verbose_name='Картинка', null=True, blank=True)
+    text = models.TextField(verbose_name="Текст статьи")
+    creator = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
+
+    def __str__(self):
+        return f'{self.title}: {self.creator.username}'
