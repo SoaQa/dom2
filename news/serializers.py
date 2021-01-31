244b04673ad94d7b0aa21e0c87298d05ad811bda
Bogdan <soaqa57@gmail.com>
2021-01-31 17:55:10 +0300

    create api

--- news/serializers.py
+++ news/serializers.py
@@ -3,12 +3,26 @@ from rest_framework import serializers
 from news.models import Novelty
 
 
+class UserSerializer(serializers.Serializer):
+    def update(self, instance, validated_data):
+        pass
+
+    def create(self, validated_data):
+        pass
+
+    email = serializers.EmailField()
+    username = serializers.CharField(max_length=100)
+
+
 class NoveltySerializer(serializers.HyperlinkedModelSerializer):
+    creator = UserSerializer(required=False)
+
     class Meta:
         model = Novelty
         fields = [
             'title',
             'text',
+            'creator',
             'created_at',
             'image_url'
         ]
